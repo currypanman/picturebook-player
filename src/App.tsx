7cfcbcc6df9596a0f3b91047a1805dea4291b565
Currypanman <currypanman.spicy@outlook.com>
2023-04-09 21:39:38 +0900

    Add opening animation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import { Opening } from './Opening';
 import { Player } from './Player';
 import './App.css';
 
+enum State {
+  Opening,
+  BookList,
+  Book,
+}
+
 function App() {
-  return (
-    <div className="App">
-      <Player />
-    </div>
-  );
+  const [state, setState] = useState(State.Opening);
+
+  switch (state) {
+    case State.Opening:
+      return (
+        <div className="App">
+          <Opening onAnimationComplete={() => setState(State.Book)}/>
+        </div>
+      );
+    case State.BookList:
+    case State.Book:
+      return (
+        <div className="App">
+          <Player />
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Opening.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import './Opening.css';
+
+type OpeningProps = {
+  onAnimationComplete: () => void;
+}
+
+function Opening(props: OpeningProps) {
+  const header = React.createRef<HTMLHeadingElement>();
+  const bear = React.createRef<HTMLImageElement>();
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      onComplete: props.onAnimationComplete
+    });
+    tl.to('.opening-header span', {x:-325 - window.innerWidth / 2, duration: 0.3, stagger: 0.3});
+    tl.to(bear.current, {y:-400, duration: 1});
+  }, [header, bear]);
+
+  return (
+    <div className="opening-container">
+      <h1 className="opening-header" ref={header}>
+        <span>クマさんと</span>
+        <span>&nbsp;</span>
+        <span>えほんを</span>
+        <span>&nbsp;</span>
+        <span>よもう!</span>
+      </h1>
+      <img className="opening-bear" ref={bear}
+        src={`${process.env.PUBLIC_URL}/present_kuma.png`} />
+    </div>
+  );
+}
+
+export { Opening };
